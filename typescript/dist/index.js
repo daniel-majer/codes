62d3f509a448b965ef8fe95dd6b9062744dae9d6
daniel-majer <dano.majer@protonmail.com>
2025-01-24 11:39:07 +0100

    codes

--- typescript/dist/index.js
+++ typescript/dist/index.js
@@ -1,2 +1,15 @@
 "use strict";
 const a = 8;
+let b = null;
+b = "a";
+function hey(greet) {
+    return greet;
+}
+let c;
+c = 8;
+c = "s";
+let boolean = 1;
+function handleInputChange(event) {
+    const inputElement = event.target;
+    console.log(inputElement.value); // Teraz máme prístup k `value`
+}
--- typescript/index.ts
+++ typescript/index.ts
@@ -1,1 +1,25 @@
 const a = 8
+
+let b = null
+
+b = "a"
+
+function hey(greet: string) {
+  return greet
+}
+
+
+let c: unknown
+
+c = 8
+
+c = "s"
+
+
+let boolean = 1 as unknown as boolean
+
+function handleInputChange(event: Event) {
+  const inputElement = event.target as unknown as HTMLInputElement
+  console.log(inputElement.value) // Teraz máme prístup k `value`
+}
+
